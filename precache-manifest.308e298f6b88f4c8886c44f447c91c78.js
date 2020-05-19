968794a75f92bcc9c53517e2efc5b641faecab4f
dinesh <chavandinesh53@gmail.com>
2020-05-19 13:22:54 +0530

    Updates

---
+++ precache-manifest.308e298f6b88f4c8886c44f447c91c78.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "012028db2d295bf3add94aab62d53618",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5293ad0c6179ec8a2d10",
+    "url": "/static/css/main.ca02da1d.chunk.css"
+  },
+  {
+    "revision": "605f0ef4b6fbc51d6422",
+    "url": "/static/js/2.74c3e006.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.74c3e006.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5293ad0c6179ec8a2d10",
+    "url": "/static/js/main.c76f1c66.chunk.js"
+  },
+  {
+    "revision": "b4867f068814a1deb1a7",
+    "url": "/static/js/runtime-main.356abe51.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
